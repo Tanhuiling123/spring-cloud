d4035ebae1ce497e25e18504e84203eeee230c2e
Tanhuiling <Tanhuiling@qq.com>
2019-04-19 16:55:46 +0800

    添加服务发现组件eureka

--- spring-cloud-disconvery-eureka/src/main/java/com/tanhui/DisconveryEurekaApplication.java
+++ spring-cloud-disconvery-eureka/src/main/java/com/tanhui/DisconveryEurekaApplication.java
@@ -2,14 +2,14 @@ package com.tanhui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class ProviderUserApplication {
+@EnableEurekaServer
+public class DisconveryEurekaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ProviderUserApplication.class,args);
+        SpringApplication.run(DisconveryEurekaApplication.class,args);
     }
 
 }
